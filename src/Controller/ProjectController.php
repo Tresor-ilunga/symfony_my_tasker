c5c6d7da147d82c112f692021e1b07b2ba8c5015
Tresor-ilunga <19im065@esisalama.org>
2023-06-12 22:39:49 +0200

    Setting up the Projects CRUD

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Project;
+use App\Form\ProjectType;
 use App\Repository\ProjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,7 +25,7 @@ class ProjectController extends AbstractController
     #[Route('/project', name: 'app_project', methods: ['GET'])]
     public function index(ProjectRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
-        return $this->render('pages/project/project.html.twig',
+        return $this->render('pages/project/index.html.twig',
             parameters: [
                 'project' => $paginator->paginate(
                     target: $repository->findAll(), //['user' => $this->getUser()]
@@ -31,5 +34,57 @@ class ProjectController extends AbstractController
                 )
             ]
         );
+    }
+
+    #[Route('/project/new', name: 'app_project_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $project = new Project();
+        $form = $this->createForm(ProjectType::class, $project);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($project);
+            $manager->flush();
+
+            $this->addFlash('success', 'Project created successfully!');
+
+            return $this->redirectToRoute('app_project');
+        }
+
+        return $this->render('pages/project/new.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/project/edit/{id}', name: 'app_project_edit', methods: ['GET', 'POST'])]
+    public function edit(Project $project, EntityManagerInterface $manager, Request $request): Response
+    {
+        $form = $this->createForm(ProjectType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($project);
+            $manager->flush();
+
+            $this->addFlash('success', 'Project updated successfully!');
+
+            return $this->redirectToRoute('app_project');
+        }
+
+        return $this->render('pages/project/edit.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/project/delete/{id}', name: 'app_project_delete', methods: ['GET', 'POST'])]
+    public function delete(Project $project, EntityManagerInterface $manager): Response
+    {
+        $manager->remove($project);
+        $manager->flush();
+
+        $this->addFlash('success', 'Project deleted successfully!');
+
+        return $this->redirectToRoute('app_project');
     }
 }
--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -1,22 +1,54 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Form;
 
 use App\Entity\Project;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
+/**
+ * Class
+ *
+ * @author tresor-ilunga <ilungat82@gmail.com>
+ */
 class ProjectType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('projectName')
-            ->add('projectLead')
-            ->add('team')
-            ->add('progress')
+            ->add('projectName', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter project name',
+                ],
+                'constraints' => [
+                    new Assert\NotNull(),
+                    new Assert\Length(min: 1, max: 50),
+                ]
+            ])
+            ->add('projectLead', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter project lead',
+                ],
+                'constraints' => [
+                    new Assert\NotNull(),
+                    new Assert\Length(min: 1, max: 50),
+                ]
+            ])
+            ->add('team', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter team',
+                ],
+            ])
+            ->add('progress', RangeType::class, [
+                'label' => 'Progress (%)',
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
